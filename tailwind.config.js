1a7c30888ba0daf1da491497b7b01f320683fe7c
WissamDouskary <wissamdouskary99@gmail.com>
2024-10-29 15:03:33 +0100

    Confige a first function on Js, Configure Tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html'],
+  theme: {
+    extend: {
+      colors: {
+        customColor: '#4ca1af',
+        hoverColor : '#36747d',
+      }
+    },
+  },
+  plugins: [],
+}
+
