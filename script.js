b305f2bb693afa016887256d3c80c51024a07a65
WissamDouskary <douskary.wissam@gmail.com>
2024-10-29 19:34:07 +0100

    add Delete task button, add X for delete task

--- script.js
+++ script.js
@@ -1,17 +1,35 @@
 let addButton = document.getElementById("addbtn");
 let addTaskInput = document.getElementById("Add-task-input");
-let Container = document.getElementById("entredTask")
+let Container = document.getElementById("entredTask");
+let elementsTask = document.getElementById("elementsTask");
+
 
 
 addButton.addEventListener("click" , function(){
     if(addTaskInput.value === ""){
         alert("please type a valid Task");
     }else{
-        let addElement = document.createElement("p");
-        console.log(addElement);
-        addElement.innerHTML = addTaskInput.value;
-        Container.appendChild(addElement);
+        let addElement = document.createElement("div");
+        addElement.innerHTML = addTaskInput.value ;
 
+        let closeBtn = document.createElement("span");
+        closeBtn.innerHTML = "x";
+        closeBtn.style.cursor = "pointer";
+
+        closeBtn.addEventListener("click" , function (){
+            elementsTask.removeChild(addElement);
+            elementsTask.removeChild(closeBtn);
+        })
+        
+        //style for elements
+        addElement.style.backgroundColor = "#608BC1"; 
+        addElement.style.borderRadius = "15px";
+        addElement.style.padding = "10px";
+        addElement.style.marginBottom = "10px";
+        
+    
+        elementsTask.appendChild(addElement);
+        elementsTask.appendChild(closeBtn);
     }
     addTaskInput.value = "";
     
