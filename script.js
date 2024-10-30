9250e96f93392b945d0d2bd1a48bfed3f6210c50
WissamDouskary <wissamdouskary99@gmail.com>
2024-10-30 08:56:43 +0100

    Change Website Design, and some other updates

--- script.js
+++ script.js
@@ -3,7 +3,9 @@ let addTaskInput = document.getElementById("Add-task-input");
 let Container = document.getElementById("entredTask");
 let elementsTask = document.getElementById("elementsTask");
 
+let inputDate = document.getElementById("input-date");
 
+console.log(inputDate.value);
 
 addButton.addEventListener("click" , function(){
     if(addTaskInput.value === ""){
@@ -13,7 +15,7 @@ addButton.addEventListener("click" , function(){
         addElement.innerHTML = addTaskInput.value ;
 
         let closeBtn = document.createElement("span");
-        closeBtn.innerHTML = "x";
+        closeBtn.innerHTML = "Delete";
         closeBtn.style.cursor = "pointer";
 
         closeBtn.addEventListener("click" , function (){
@@ -22,18 +24,18 @@ addButton.addEventListener("click" , function(){
         })
         
         //style for elements
-        addElement.style.backgroundColor = "#608BC1"; 
-        addElement.style.borderRadius = "15px";
+        addElement.style.backgroundColor = "#FF3232";
+        addElement.style.borderRadius = "6px";
         addElement.style.padding = "10px";
         addElement.style.marginBottom = "10px";
         
 
-        closeBtn.style.backgroundColor = "#D9D9D9";
-        closeBtn.style.color = "red";
-        closeBtn.style.padding = "5px";
-        closeBtn.style.borderRadius = "10px"
+        // closeBtn.style.backgroundColor = "#D9D9D9";
+        // closeBtn.style.color = "red";
+        // closeBtn.style.padding = "5px";
+        // closeBtn.style.borderRadius = "10px"
     
-        
+
         elementsTask.appendChild(addElement);
         elementsTask.appendChild(closeBtn);
     }
